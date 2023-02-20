ffacd820b352b83be4e66798eedafe46998044ef
Alexeykormiltsev <cormiltsev@yandex.ru>
2023-02-20 16:41:47 +0500

    начал делать адаптив

--- Template/js/main.js
+++ Template/js/main.js
@@ -1,7 +1,19 @@
 $(function () {
+    $('.menu__btn').on('click', function () {
+        $('.header__top-nav').toggleClass('header__top-nav--active');
+        $('.header__nav').toggleClass('header__nav--active');
+        $('.menu__btn').toggleClass('menu__btn--active');
+        $('body').toggleClass('hidden');
+        $('.main').toggleClass('main--active');
+      });
+
+
+
     $('.slider').slick({
         dots: false,
-        speed: 700,
+        autoplay: true,
+        autoplaySpeed: 7000,
+        speed: 1200,
 
         prevArrow: '<button type="button" class="slick-prev"><img src="images/arrow-left.svg" alt="arrow-left"></button>',
 
